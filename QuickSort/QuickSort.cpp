2744fb40fbb1a395b3ae5600e3bcb3d91c475d2f
WildanLikeTamago <utoyowildan13039@gmail.com>
2023-03-31 16:06:28 +0700

    Menampilkan Angka

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -75,6 +75,15 @@ void display() {
 	cout << "\n\nNumber of comparison: " << cmp_count << endl;
 	cout << "Number of movement: " << mov_count << endl;
 }
+
+int main() {
+	input();
+	q_short(0, n - 1);
+	display();
+	system("pause");
+
+	return 0;
+}
 	
 
 
